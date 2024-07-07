5a321ff441126e919236b1f940c01bd72fb8b68b
moattx <wowzers1lol@gmail.com>
2024-07-07 07:24:03 +0300

    a

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,5 +1,5 @@
 <footer class="flex justify-end content-center w-screen bg-black text-white">
     <p>جميع الحقوق محفوظة ,2024, مسجلة</p>
 
-<!--    <p> .مسجلة.   كل الحقوق محفوظة &copy;</p> -->
+<!--    <p> مسجلة.   كل الحقوق محفوظة &copy;</p> -->
 </footer>
