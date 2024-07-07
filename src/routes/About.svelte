813c2910486c0239bdc5ab5f68452743997b78fe
moattx <wowzers1lol@gmail.com>
2024-07-07 11:10:37 +0300

    Make the transitions smoother

--- src/routes/About.svelte
+++ src/routes/About.svelte
@@ -47,12 +47,12 @@
     {#each items as item}
     <!--<div class="flex flex-row items-center justify-center w-screen box-border h-32 p-4 border-4">-->
     <div bind:this={observedElement}  class="flex flex-col justify-center items-center  box-border h-40 p-4 border-4">
-        {#if isVisible}
-        <div 	in:fly={{ delay: 0, duration: 300, x: 0, y: 100, opacity: 0.5, easing: quintOut }} class="text-center  text-[3rem] text-rose-600">
+      {#if isVisible}
+        <div 	in:fly={{ delay: 0, duration: 300, x: 0, y: 200, opacity: 0.5, easing: quintOut }} class="text-center  text-[3rem] text-rose-600">
         <span  class="{item[0]}"></span>
         </div>
 
-        <div 	transition:fly={{ delay: 250, duration: 300, x: 100, y: 500, opacity: 0.5, easing: quintOut }}  class="text-center gap-1">
+        <div 	transition:fly={{ delay: 0, duration: 300, y: 200, opacity: 0.5, easing: quintOut }}  class="text-center gap-1">
         <h2 class="font-semibold text-xl mb-2">{item[1]}</h2>
         <p class="text-base leading-relaxed">{item[2]}</p>
         </div>
