a1b6547f2dc55e0ec1c310c8eade4fc6f0110843
moattx <wowzers1lol@gmail.com>
2024-12-25 13:35:32 -0500

    fix icons not showing in about

--- src/routes/About.svelte
+++ src/routes/About.svelte
@@ -34,7 +34,7 @@
     import { fly } from 'svelte/transition';
 	import { quintOut } from 'svelte/easing';
 
-    const items = [["info",  "من نحن", ".نحن مجموعة ملتزمون تمامًا بتقديم أفضل التطبيقات التقنية التي يقدمها هذا العالم"], ["eye", "رؤيتنا", ".أن نكون المزود الرائد للتطبيقات الموثوقة والمبتكرة والفعالة من حيث التكلفة في المملكة العربية السعودية"], ["bullseye", "أهدافنا", ".مهمتنا هي تقديم تطبيقات عالية الجودة وبأسعار معقولة"]]
+    const items = [["info-circle",  "من نحن", ".نحن مجموعة ملتزمون تمامًا بتقديم أفضل التطبيقات التقنية التي يقدمها هذا العالم"], ["search", "رؤيتنا", ".أن نكون المزود الرائد للتطبيقات الموثوقة والمبتكرة والفعالة من حيث التكلفة في المملكة العربية السعودية"], ["bullseye", "أهدافنا", ".مهمتنا هي تقديم تطبيقات عالية الجودة وبأسعار معقولة"]]
     //const items = [["fa fa-info-circle", "TITLE", "INFO"],["fa fa-info-circle", "TITLE", "INFO"],["fa fa-info-circle", "TITLE", "INFOaaaaaa"]]
 </script>
 
@@ -48,22 +48,9 @@
     <!--<div class="flex flex-row items-center justify-center w-screen box-border h-32 p-4 border-4">-->
     <div bind:this={observedElement}  class="flex flex-col justify-center items-center  box-border h-40 p-4 border-4">
       {#if isVisible}
-        <div 	in:fly={{ delay: 0, duration: 300, x: 0, y: 200, opacity: 0.5, easing: quintOut }} class="text-center  text-[3rem] text-rose-600">
-<!--<span  class="{item[0]}"></span>-->
-{#if item[0] == "info"}
-  <!--<img src="../envelope-solid.svg">-->
-  <!--<box-icon type='solid' name='info-circle' height="100" width=100></box-icon>-->
-  <object type="image/svg+xml" data="src/info.svg" width="100" height="75"></object>
-
-{:else if item[0] == "eye"}
-  <object type="image/svg+xml" data="src/eye.svg" width="100" height="75"></object>
-{:else}
-  <object type="image/svg+xml" data="src/bullseye.svg" width="100" height="75"></object>
-  <!--<box-icon name='bullseye' type='solid' color='#e23c3c' ></box-icon>-->
-
-{/if}
+        <div 	in:fly={{ delay: 0, duration: 300, x: 0, y: 200, opacity: 0.5, easing: quintOut }} class="text-center">
+  <box-icon type='solid' name='{item[0]}'></box-icon>
         </div>
-
         <div 	transition:fly={{ delay: 0, duration: 300, y: 200, opacity: 0.5, easing: quintOut }}  class="text-center gap-1">
         <h2 class="font-semibold text-xl mb-2">{item[1]}</h2>
         <p class="text-base leading-relaxed">{item[2]}</p>
